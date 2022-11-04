4fc71df53b0804511e38b6c74f5ed7641bdf8740
SirmaXX <sea-97@hotmail.com>
2022-11-04 23:46:48 +0300

    requirement eklendi

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # adb-web-app
 
+# İhtiyaçlar
+ Kullanıcı tarafından IP yazıldığı vakit aşağıdaki komutlar butonlar ile çalışacaktır.
+ - SHUTDOWN,
+ - RESTART
+ - APP CLOSE
+ - APP OPEN
+ - APP INSTALL
+ - APP UNINSTALL
+ - APP LİST 
+
 
 ## döküman oluşturma
-pdoc --html ./deneme.py -o ./Docs
+pdoc --html ./lib.py -o ./Docs --force
--- lib.py
+++ lib.py
@@ -1,13 +1,50 @@
 import subprocess
+import os
 
 
 
-def getdevices():
+
+def StartServer():
  """
-    cihazları gösteren yapı
+    server başlatan fonksiyon
+ """
+ subprocess.run(["adb", "start-server"])
+
+
+def KillServer():
+ """
+    server durduran fonksiyon
+ """
+ subprocess.run(["adb", "kill-server"])
+
+
+
+def GetDevices():
+ """
+    cihazları gösteren fonksiyon
  """
  subprocess.run(["adb", "devices"])
 
 
 
-getdevices()
+def GetDevicesDetailed():
+ """
+    cihazları gösteren detaylı fonksiyon
+ """
+ subprocess.run(["adb", "devices","-l"])
+
+
+
+def PackageList():
+   """
+    yüklü paketleri gösteren fonksiyon
+   """
+   cmd = "adb shell pm list packages"
+   returned_value = os.system(cmd)  # returns the exit code in unix
+   print('returned value:', returned_value)
+
+ 
+
+
+
+StartServer()
