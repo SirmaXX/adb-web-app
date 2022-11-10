c8694c1a27c70ed648b94f687f76e09c0daaf334
SirmaXX <sea-97@hotmail.com>
2022-11-10 05:26:58 +0300

    txt dosyası üzerinden islemler ayarlandı

--- shell.sh
+++ shell.sh
@@ -1,12 +1,35 @@
 #!/bin/bash
-testA() {
-  echo "TEST A";
-}
 
 
 ListDevices(){ adb devices; }
 
 DetailDevices(){ adb devices -l; }
 
+
+#source shell.sh ;ListPackages
 ListPackages(){ 
-    adb shell pm list packages; }
+  
+   adb shell pm list packages ;
+   }
+
+#source shell.sh ;ListPackagesWFile
+ListPackagesWFile(){ 
+  
+   adb shell pm list packages  | sed 's/package://i' | tee packageslist.txt
+
+   }
+InstallApp(){
+adb install  -g $1;
+}
+
+
+UninstallApp(){
+adb uninstall $1;
+}
+
+#source shell.sh ;UninstallAppWFile
+UninstallAppWFile(){
+
+   while read line; do adb uninstall $line; done < removelist.txt
+}
+
